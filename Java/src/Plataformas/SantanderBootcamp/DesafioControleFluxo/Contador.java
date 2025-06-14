5f5ca9fbe34a649b8d2817835b02ded9351e948e
alangonno <alannogueira314@gmail.com>
2025-06-14 17:28:55 -0300

    pequena alteração nos atributos

--- Java/src/Plataformas/SantanderBootcamp/DesafioControleFluxo/Contador.java
+++ Java/src/Plataformas/SantanderBootcamp/DesafioControleFluxo/Contador.java
@@ -5,21 +5,17 @@ import Plataformas.SantanderBootcamp.DesafioControleFluxo.exceptions.ParametroIn
 import java.util.Scanner;
 
 public class Contador {
-
-    private static int n1;
-    private static int n2;
-
     public static void main(String[] args) {
 
         try (Scanner entrada = new Scanner(System.in)) {
             System.out.println("Digite o primeiro parâmetro");
-            n1 = entrada.nextInt();
+            int n1 = entrada.nextInt();
             entrada.nextLine();
             System.out.println("Digite o segundo parâmetro");
-            n2 = entrada.nextInt();
+            int n2 = entrada.nextInt();
             entrada.nextLine();
             try{
-                int n3 = getN3();
+                int n3 = getN3(n1, n2);
                 for (int n = 0; n < n3; n += 1 ) {
                     System.out.printf("Imprimindo o número %d\n", n + 1);
                 }
@@ -31,7 +27,7 @@ public class Contador {
         }
     }
 
-    private static int  getN3() {
+    private static int  getN3(int n1, int n2) {
         if(n1 > n2) throw new ParametroInvalidosException("O segundo numero deve ser maior que o primeiro");
         int n3 = n2 - n1;
         return n3;
