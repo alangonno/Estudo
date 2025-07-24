2694c26422919ae5e67dcc409766ed7a031aae58
Alan Gonçalves <alannogueira314@gmail.com>
2025-07-24 16:32:20 -0300

    delete mysql

--- MySql/banco-relacional/atualizarEstados.sql
+++ MySql/banco-relacional/atualizarEstados.sql
@@ -3,4 +3,4 @@ set nome = 'Errejota',
     populacao = 20.2 
 where sigla = 'RJ';
 
-select nome, populacao from estados est where sigla = 'RJ';
+select nome, populacao from estados est where sigla = 'RJ'; 
---
+++ MySql/banco-relacional/inserirEstado3.sql
@@ -0,0 +1,6 @@
+INSERT INTO estados (id, nome, sigla, regiao, populacao)
+values(1000, 'Novo', 'NV', 'Sul', 0.2);
+
+insert into estados(nome, sigla, regiao, populacao)
+values('Mais Novo', 'MV', 'Norte', 2.54)
+
