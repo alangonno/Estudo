faf1c59a4c65b8054c5337daabe00a063c2ca15f
Alan Gonçalves <alannogueira314@gmail.com>
2025-07-25 12:05:39 -0300

    relação de tabelas

--- MySql/banco-relacional/lerEstados.sql
+++ MySql/banco-relacional/lerEstados.sql
@@ -1,9 +1,9 @@
 SELECT * from estados
 
-SELECT nome, sigla as 'Siglas 2 letras' from estados 
-where nome = 'Acre'
+-- SELECT nome, sigla as 'Siglas 2 letras' from estados 
+-- where nome = 'Acre'
 
 
-SELECT nome, sigla as 'Siglas 2 letras' from estados 
-where populacao >= 10
-order by populacao desc --Ordenaçao Descrescente
+-- SELECT nome, sigla as 'Siglas 2 letras' from estados 
+-- where populacao >= 10
+-- order by populacao desc --Ordenaçao Descrescente
