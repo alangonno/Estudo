a4aa315e063038f596210f9b6ac2222a7ba984de
Alan Gonçalves <alannogueira314@gmail.com>
2025-07-28 16:53:10 -0300

    Criando conexões e DATABASE com JDBC

---
+++ MySql/JDBC/jdbc/src/App.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) throws SQLException {
+        final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSLL=true";
+        final String usuario = "root";
+        final String senha = "C3cbczzZ*";
+        
+        Connection conexao = DriverManager.getConnection(url, usuario, senha);
+        System.out.println("COnexão feita");
+        conexao.close();
+
+    }
+}
